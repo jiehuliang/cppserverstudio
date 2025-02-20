6949d353993b53b763d5f9375599b6a6a2cb869b
LiangJiehu <0049003844@znv.com>
2025-02-21 00:39:59 +0800

    修改H264Rtp文件名

---
+++ src/Rtsp/H264Rtp.cpp
@@ -0,0 +1,147 @@
+#include "H264Rtp.h"
+#include <arpa/inet.h>
+#include "Logging.h"
+
+
+int H264RtpDecoder::inputRtp(const RtpPacket::Ptr& rtp) {
+	auto seq = rtp->getSeq();
+	auto ret = decodeRtp(rtp);
+}
+
+int H264RtpDecoder::decodeRtp(const RtpPacket::Ptr& rtp) {
+	
+}
+
+
+H264RtpEncoder::H264RtpEncoder(uint32_t ssrc, uint8_t interleaved, uint8_t pt, uint32_t sample_rate, size_t mtu_size) {
+    _pt = pt;
+    _interleaved = interleaved;
+    _ssrc = ssrc;
+    _sample_rate = sample_rate;
+    _mtu_size = mtu_size;
+}
+
+void H264RtpEncoder::insertConfigFrame(uint32_t pts) {
+    if (!sps_ || !pps_) {
+        return;
+    }
+    //gop缓存从sps开始，sps、pps后面还有时间戳相同的关键帧，所以mark bit为false
+    packRtp(sps_->buffer.c_str(), sps_->len, pts, false, true);
+    packRtp(pps_->buffer.c_str(), sps_->len, pts, false, false);
+}
+
+int H264RtpEncoder::inputFrame(const H264Nalu::Ptr& nalu) {
+    switch (nalu->nal_unit_type) {
+    case H264Nalu::H264NaluType::NAL_SPS: {
+        sps_ = std::move(nalu);
+        return true;
+    }
+    case H264Nalu::H264NaluType::NAL_PPS: {
+        pps_ = std::move(nalu);
+        return true;
+    }
+    default: break;
+    }
+    if(nalu->dropAble()) {
+        return true;
+    }
+    if (last_nalu_) {
+        //如果时间戳发生了变化，那么markbit才置true
+        inputFrame_l(last_nalu_, last_nalu_->_pts != nalu->_pts);
+    }
+    last_nalu_ = std::move(nalu);
+}
+
+bool H264RtpEncoder::inputFrame_l(const H264Nalu::Ptr& nalu, bool is_mark){
+    if (nalu->keyFrame()) {
+        //保证每一个关键帧前都有SPS与PPS
+        insertConfigFrame(nalu->_pts);
+    }
+    packRtp(nalu->buffer.c_str(), nalu->len, nalu->_pts, is_mark, false);
+    return true;
+}
+
+void H264RtpEncoder::packRtp(const char* data, size_t len, uint32_t pts, bool is_mark, bool gop_pos){
+    if (len + 3 <= _mtu_size - RtpPacket::RtpHeaderSize) {
+        packRtpStapA(data, len, pts, is_mark, gop_pos);
+    } else {
+        packRtpFu(data, len, pts, is_mark, gop_pos);
+    }
+}
+
+void H264RtpEncoder::packRtpStapA(const char* ptr, size_t len, uint32_t pts, bool is_mark, bool gop_pos) {
+    auto rtp = makeRtp(0, nullptr, len + 3, is_mark, pts);
+    uint8_t* playload = rtp->getPayload();
+    playload[0] = (ptr[0] & (~0x1F)) | 24;
+    playload[1] = (len >> 8) & 0xFF;
+    playload[2] = len & 0xFF;
+    memcpy(playload + 3 , (uint8_t*)ptr, len);
+    send_cb_(rtp);
+}
+
+void H264RtpEncoder::packRtpFu(const char* ptr, size_t len, uint32_t pts, bool is_mark, bool gop_pos) {
+    auto packet_size = _mtu_size - RtpPacket::RtpHeaderSize - 2;
+
+    auto fu_flag_0 = (ptr[0] & (~0x1F)) | 28;
+    auto fu_flag_1 = (ptr[0] & 0x1F);
+
+    //FU-A start bit
+    fu_flag_1 |= 0x80;
+
+    size_t offset = 1;
+    while (!(fu_flag_1 & 0x40)) {
+        if (!(fu_flag_1 & 0x80) && len <= offset + packet_size) {
+            //FU-A end bit
+            fu_flag_1 |= 0x40;
+            packet_size = len - offset;
+        }
+
+        auto rtp = makeRtp(0, nullptr, packet_size + 2, (fu_flag_1 & 0x40) && is_mark, pts);
+        uint8_t* playload = rtp->getPayload();
+        playload[0] = fu_flag_0;
+        playload[1] = fu_flag_1;
+        memcpy(playload + 2, ptr + offset, packet_size);
+        send_cb_(rtp);
+
+        offset += packet_size;
+        fu_flag_1 &= (~0x80);
+    }
+}
+
+
+RtpPacket::Ptr H264RtpEncoder::makeRtp(int type,const char* data,size_t len,bool mark,uint32_t stamp) {
+    uint16_t playload_len = (uint16_t)(len + RtpPacket::RtpHeaderSize);
+    RtpPacket::Ptr rtp = RtpPacket::CreateRtp();
+    //rtsp over tcp 头
+    auto pack = rtp->getData();
+    std::string capacity(RtpPacket::RtpTcpHeaderSize + playload_len, 0);
+    pack->Append(capacity.c_str(), RtpPacket::RtpTcpHeaderSize + playload_len);
+    auto ptr = pack->Peek();
+    ptr[0] = '$';
+    ptr[1] = _interleaved;
+    ptr[2] = playload_len >> 8;
+    ptr[3] = playload_len & 0xFF;
+
+    auto header = rtp->getHeader();
+    header->version = RtpPacket::RtpVersion;
+    header->padding = 0;
+    header->ext = 0;
+    header->csrc = 0;
+    header->mark = mark;
+    header->pt = _pt;
+    header->seq = _seq;
+    ++_seq;
+    header->timestamp = uint64_t(stamp);
+    header->ssrc = _ssrc;
+
+    //有效负载
+    if (data) {
+        memcpy(&ptr[RtpPacket::RtpHeaderSize + RtpPacket::RtpTcpHeaderSize], data, len);
+    }
+    return rtp;
+}
+
+void H264RtpEncoder::set_send_cb(const std::function<void(const RtpPacket::Ptr& rtp)>& send_cb) {
+    send_cb_ = send_cb;
+}
+
---
+++ src/Rtsp/H264Rtp.h
@@ -0,0 +1,63 @@
+#ifndef H264_RTP_H
+#define H264_RTP_H
+#include <cstdint>
+#include <cstddef>
+#include <functional>
+#include "Rtsp.h"
+#include "H264.h"
+
+class H264RtpDecoder {
+public:
+	int inputRtp(const RtpPacket::Ptr& rtp);
+
+private:
+	int decodeRtp(const RtpPacket::Ptr& rtp);
+
+	bool singleFrame();
+	bool unpackStapA();
+	bool unpackStapB();
+	bool unpackMtap16();
+	bool unpackMtap24();
+	bool unpackFuA();
+	bool unpackFuB();
+
+	void outputFrame();
+};
+
+
+class H264RtpEncoder {
+public:
+	H264RtpEncoder(uint32_t ssrc, uint8_t interleaved, uint8_t pt = 96, uint32_t sample_rate = 90000, size_t mtu_size = 1400);
+	~H264RtpEncoder() {}
+
+	int inputFrame(const H264Nalu::Ptr& nalu);
+
+	void set_send_cb(const std::function<void(const RtpPacket::Ptr& rtp)>& send_cb);
+private:
+	void insertConfigFrame(uint32_t pts);
+	bool inputFrame_l(const H264Nalu::Ptr& nalu, bool is_mark);
+	void packRtp(const char* data, size_t len, uint32_t pts, bool is_mark, bool gop_pos);
+	void packRtpFu(const char* ptr, size_t len, uint32_t pts, bool is_mark, bool gop_pos);
+	void packRtpStapA(const char* ptr,size_t len,uint32_t pts,bool is_mark,bool gop_pos);
+	RtpPacket::Ptr makeRtp(int type, const char* data, size_t len, bool mark, uint32_t stamp);
+
+
+
+	std::function<void(const RtpPacket::Ptr& rtp)> send_cb_ = [](const RtpPacket::Ptr& rtp) {};
+
+private:
+	H264Nalu::Ptr sps_;
+	H264Nalu::Ptr pps_;
+	H264Nalu::Ptr last_nalu_;
+
+private:
+	uint8_t _pt;
+	uint8_t _interleaved;
+	uint16_t _seq = 0;
+	uint32_t _ssrc;
+	uint32_t _sample_rate;
+	size_t _mtu_size;
+};
+
+
+#endif // H264_RTP_H
