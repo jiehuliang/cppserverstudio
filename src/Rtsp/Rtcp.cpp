2a4f5bcf4428e811827f59bf7dd4bcc16cab3e5f
LiangJiehu <0049003844@znv.com>
2025-03-04 00:58:36 +0800

    rtcp封装类定义部分实现

--- src/Rtsp/Rtcp.cpp
+++ src/Rtsp/Rtcp.cpp
@@ -1,1 +1,11 @@
 #include "Rtcp.h"
+
+static size_t alignSize(size_t bytes) {
+    return (size_t) ((bytes + 3) >> 2) << 2;
+}
+
+
+std::shared_ptr<RtcpSR> RtcpSR::create(size_t item_count){
+    auto real_size = sizeof(RtcpSR) - sizeof(ReportItem) + item_count * sizeof(ReportItem);
+    auto bytes=alignSize(real_size);
+}
